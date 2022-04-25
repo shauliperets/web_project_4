604dea777cfedddaebdf09839f78e4ff401a405c
shauliperets <shauliperets@gmail.com>
2022-04-25 13:42:52 +0300

    Add popup

---
+++ js/index.js
@@ -0,0 +1,51 @@
+let closeButton = document.querySelector(".popup__close-button");
+
+let editButton = document.querySelector(".popup__edit-button");
+
+let saveButton = document.querySelector(".popup__save-button");
+
+function handleEditButtonClick() {
+  console.log("edit clicked");
+
+  let popup = document.querySelector(".popup");
+
+  popup.classList.toggle("popup__display");
+
+  let title = document.querySelector(".profile__title");
+
+  let subtitle = document.querySelector(".profile__subtitle");
+
+  let inputs = document.querySelectorAll(".popup__input");
+
+  inputs[0].value = title.textContent;
+  inputs[1].value = subtitle.textContent;
+}
+
+function handleCloseButtonClick() {
+  console.log("close clicked");
+
+  let popup = document.querySelector(".popup");
+
+  popup.classList.toggle("popup__display");
+}
+
+function handleSaveButtonClick() {
+  console.log("save clicked");
+
+  let inputs = document.querySelectorAll(".popup__input");
+
+  let title = document.querySelector(".profile__title");
+
+  let subtitle = document.querySelector(".profile__subtitle");
+
+  let popup = document.querySelector(".popup");
+
+  title.textContent = inputs[0].value;
+  subtitle.textContent = inputs[1].value;
+
+  popup.classList.toggle("popup__display");
+}
+
+closeButton.addEventListener("click", handleCloseButtonClick);
+editButton.addEventListener("click", handleCloseButtonClick);
+saveButton, addEventListener("click", handleSaveButtonClick);
