f6be87c67f5a51d36094ab2ecfd5c583c0c8547d
shauliperets <shauliperets@gmail.com>
2022-04-28 13:00:43 +0300

    bag: popup closed

--- js/index.js
+++ js/index.js
@@ -4,50 +4,33 @@ let editButton = document.querySelector(".profile__edit-button");
 
 let saveButton = document.querySelector(".popup__save-button");
 
+let popup = document.querySelector(".popup");
+
+let title = document.querySelector(".profile__title");
+
+let subtitle = document.querySelector(".profile__subtitle");
+
+let inputs = document.querySelectorAll(".popup__input");
+
 function handleEditButtonClick() {
-  console.log("edit clicked");
+  popup.classList.toggle("popup_display");
 
-  let popup = document.querySelector(".popup");
-
-  popup.classList.toggle("popup__display");
-
-  let title = document.querySelector(".profile__title");
-
-  let subtitle = document.querySelector(".profile__subtitle");
-
-  let inputs = document.querySelectorAll(".popup__input");
-
-  inputs[0].value = title.textContent;
-  inputs[1].value = subtitle.textContent;
-
-  console.log("edit clicked close");
+  document.getElementById("popup-name").value = title.textContent;
+  document.getElementById("popup-about-me").value = subtitle.textContent;
 }
 
 function handleCloseButtonClick() {
-  console.log("close clicked");
-
-  let popup = document.querySelector(".popup");
-
-  popup.classList.toggle("popup__display");
+  popup.classList.toggle("popup_display");
 }
 
 function handleSaveButtonClick() {
-  console.log("save clicked");
-
-  let inputs = document.querySelectorAll(".popup__input");
-
-  let title = document.querySelector(".profile__title");
-
-  let subtitle = document.querySelector(".profile__subtitle");
-
-  let popup = document.querySelector(".popup");
-
   title.textContent = inputs[0].value;
   subtitle.textContent = inputs[1].value;
 
-  popup.classList.toggle("popup__display");
+  popup.classList.toggle("popup_display");
 }
 
+/*
 function handleIconClick(id) {
   if (document.getElementById(id).src.includes("/heart.svg")) {
     document.getElementById(id).src = "./images/heart-black.svg";
@@ -55,7 +38,8 @@ function handleIconClick(id) {
     document.getElementById(id).src = "./images/heart.svg";
   }
 }
+*/
 
 closeButton.addEventListener("click", handleCloseButtonClick);
 editButton.addEventListener("click", handleEditButtonClick);
-saveButton.addEventListener("click", handleSaveButtonClick);
+/*saveButton.addEventListener("click", handleSaveButtonClick);*/
